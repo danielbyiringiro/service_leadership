c77201b7e4889a721d3934efe214447f57ffe434
danielbyiringiro <daniel.byiringiro@ashesi.edu.gh>
2024-07-09 17:53:04 +0000

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Signin from './components/Signin';
 import Home from './components/Home';
 import Gallery from './components/Gallery';
 import Scholarship from './components/Scholarship';
+import Ashesi from './components/ashesi';
+import Legon from './components/legon';
+import KNUST from './components/knust'
+import Academic from './components/academic';
 
 export default () => {
   return (
@@ -14,6 +18,10 @@ export default () => {
         <Route path="/home" element={<Home />} />
         <Route path="/scholarship" element={<Scholarship />} />
         <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/ashesi" element={<Ashesi/>} />
+        <Route path="/knust" element={<KNUST/>} />
+        <Route path="/legon" element={<Legon/>} />
+        <Route path="/academic" element={<Academic/>} />
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import './Home.css'
 import UniGhanaLogo from '../assets/unighana.png'
+import HomeImage from '../assets/home_image.png'
+import Ashesi from '../assets/ashesi.png'
+import KNUST from '../assets/knust.png'
+import Legon from '../assets/legon.png'
+import Academic from '../assets/academic_city.png'
 import { Link } from "react-router-dom"
 import Signin from './Signin'
 
@@ -23,17 +28,40 @@ export default () =>
                     </div>
                 </div>
                 <div className='Middle'>
-                    <p>Picture</p>
-                    <p>Higher Education at your doorstep</p>
-                    <p>Ensure your future with us no more waiting</p>
-                    <button>Get Started</button>
+                    <div className='image-container'>
+                        <img src={HomeImage}/>
+                        <div className='overlay-text'>
+                            <p id="middle_text_first">Higher Education</p>
+                            <p id="middle_text_second">-at your doorstep</p>
+                            <p id="middle_text_third">Explore your future with us now. No more waiting.</p>
+                            <button id="middle_button">Get Started</button>
+                        </div>
+                    </div>
                 </div>
                 <div className='Last'>
-                    <p>Find the university of your dream</p>
-                    <p>Ashesi</p>
-                    <p>KNUST</p>
-                    <p>Legon</p>
-                    <p>Academic City</p>
+                    <p id="last_first">Find the university of your dreams</p>
+                    <div className='university-grid'>
+                        <div className='university-card ashesi'>
+                            <Link className="university-name" to="/ashesi">Ashesi University</Link>
+                            <p className="university-description">Explore Ashesi University with us now. From its campus to programs offered.</p>
+                            <img src={Ashesi} alt="Ashesi University" />
+                        </div>
+                        <div className='university-card knust'>
+                            <Link className="university-name" to="/knust">Kwame Nkrumah University of Science and Technology</Link>
+                            <p className="university-description">Explore KNUST, the region of Gold with us now. From its campus to programs offered.</p>
+                            <img src={KNUST} alt="KNUST" />
+                        </div>
+                        <div className='university-card academic'>
+                            <Link className="university-name" to="/academic">Academic City University College</Link>
+                            <p className="university-description">Explore Academic City with us now. From its campus to programs offered.</p>
+                            <img src={Academic} alt="Academic City" />
+                        </div>
+                        <div className='university-card legon'>
+                            <Link className="university-name" to="/legon">University of Ghana</Link>
+                            <p className="university-description">Explore the premier university with us now. From its campus to programs offered.</p>
+                            <img src={Legon} alt="University of Ghana" />
+                        </div>
+                    </div>
                 </div>
             </div>
             </>
